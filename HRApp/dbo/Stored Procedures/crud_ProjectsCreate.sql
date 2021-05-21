80fdc2e955a8eeeba38dfc41384102f316f4c5f8
Serhii <avakyanms@gmail.com>
2021-05-21 19:25:44 +0300

    Added Stored Procedures

---
+++ HRApp/dbo/Stored Procedures/crud_ProjectsCreate.sql
@@ -0,0 +1,23 @@
+﻿CREATE PROCEDURE [dbo].[crud_ProjectsCreate]
+    @Title nvarchar(255),
+    @Description nvarchar(255),
+    @DirectionID int
+AS
+BEGIN
+INSERT INTO Projects  (
+    Title,
+    Description,
+	DirectionID)
+    VALUES (
+    @Title,
+    @Description,
+    @DirectionID)
+ DECLARE @ID INT;
+SET @ID = SCOPE_IDENTITY()
+SELECT 
+    Title = @Title,
+    Description = @Description,
+    DirectionID=@DirectionID
+FROM Positions 
+WHERE  ID = @ID
+END
---
+++ HRApp/dbo/Stored Procedures/crud_ProjectsDelete.sql
@@ -0,0 +1,8 @@
+﻿CREATE PROCEDURE [dbo].[crud_ProjectsDelete]
+	 @ID int
+AS 
+BEGIN 
+DELETE
+FROM   Projects
+WHERE  ID = @ID
+END
