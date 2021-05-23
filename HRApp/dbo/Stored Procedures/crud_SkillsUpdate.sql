b1ee7f5b31c07d9acbebb9bfe0c7dd50e5cc2546
Rostislav <skochmen@gmail.com>
2021-05-24 01:01:31 +0300

    Add CRUD Skills table

---
+++ HRApp/dbo/Stored Procedures/crud_SkillsUpdate.sql
@@ -0,0 +1,12 @@
+﻿CREATE PROCEDURE [dbo].[crud_SkillsUpdate]
+	@ID int,
+	@title nvarchar(255),
+	@description nvarchar(255)
+AS
+
+	UPDATE [dbo].[Skills]
+	SET 
+	[dbo].[Skills].Title =@title,
+	[dbo].[Skills].Description = @description
+
+	WHERE [dbo].[Skills].ID = @ID
