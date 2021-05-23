7837736080d4a641efc427b45d8dc98b4e28f7b1
Rostislav <skochmen@gmail.com>
2021-05-24 01:14:50 +0300

    Add CRUD Companies_Depatments table

---
+++ HRApp/dbo/Stored Procedures/crud_Companies_DepatmentsRead.sql
@@ -0,0 +1,5 @@
+﻿CREATE PROCEDURE [dbo].[crud_Companies_DepatmentsRead]
+	@ID int 
+AS
+	SELECT * FROM [dbo].[Companies_Depatments]
+	WHERE [dbo].[Companies_Depatments].ID = @ID
---
+++ HRApp/dbo/Stored Procedures/crud_Companies_DepatmentsUpdate.sql
@@ -0,0 +1,12 @@
+﻿CREATE PROCEDURE [dbo].[crud_Companies_DepatmentsUpdate]
+	@ID int,
+	@companyID int,
+	@departmentID int
+AS
+	DELETE FROM [dbo].[Companies_Depatments]
+	WHERE [dbo].[Companies_Depatments].ID = @ID
+
+	INSERT INTO [dbo].[Companies_Depatments]
+	VALUES(
+	@companyID,
+	@departmentID)
