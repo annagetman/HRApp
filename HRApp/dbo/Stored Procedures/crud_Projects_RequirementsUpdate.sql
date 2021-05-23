114917a9045f0b808de99ee817d6fa403644195b
Rostislav <skochmen@gmail.com>
2021-05-24 00:43:02 +0300

    Added CRUD

---
+++ HRApp/dbo/Stored Procedures/crud_Projects_RequirementsUpdate.sql
@@ -0,0 +1,11 @@
+﻿CREATE PROCEDURE [dbo].[crud_Projects_RequirementsUpdate]
+	@projectID int,
+	@requirementsID int
+AS
+
+DELETE FROM [dbo].[Projects_Requirements]
+WHERE ([dbo].[Projects_Requirements].ProjectID = @projectID
+and
+[dbo].[Projects_Requirements].RequirementsID = @requirementsID)
+INSERT INTO [dbo].[Projects_Requirements]
+Values (@projectID,@requirementsID)
---
+++ HRApp/dbo/Stored Procedures/crud_RequirementsUpdate.sql
@@ -0,0 +1,11 @@
+﻿CREATE PROCEDURE [dbo].[crud_RequirementsUpdate]
+	@ID int,
+	@skillID int,
+	@levelSkillID int,
+	@amountEmployes int
+AS
+	UPDATE [dbo].[Requirements]
+	SET SkillID = @skillID,
+	LevelSkillsID = @levelSkillID,
+	AmountEmployes = @amountEmployes
+	WHERE ID = @ID
